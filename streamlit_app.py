12399b6a2216363ce208073cb92a78c1bde16ca0
abhishek mutkekar <84004361+H22arry@users.noreply.github.com>
2024-09-26 12:30:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd 
+
 
 st.title('🎈 Machine Learning')
-
+dp = read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv
 st.info('Hello world!')
